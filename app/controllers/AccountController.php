59b7ecd1a4fa469f523f66e5d349033e870cc67e
fathiaauliaputri <byefathia@gmail.com>
2022-11-21 14:44:09 +0700

    first commit

---
+++ app/controllers/AccountController.php
@@ -0,0 +1,33 @@
+<?php 
+/**
+ * Account Page Controller
+ * @category  Controller
+ */
+class AccountController extends SecureController{
+	function __construct(){
+		parent::__construct(); 
+		$this->tablename = "buku";
+	}
+	/**
+     * Change account email
+     * @return BaseView
+     */
+	function change_email($formdata = null){
+		if($formdata){
+			$email = trim($formdata['']);
+			$db = $this->GetModel();
+			$rec_id = $this->rec_id = USER_ID; //get current user id from session
+			$tablename = $this->tablename;
+			$db->where ("id", $rec_id);
+			$result = $db->update($tablename, array('' => $email ));
+			if($result){
+				$this->set_flash_msg("Email address changed successfully", "success");
+				$this->redirect("account");
+			}
+			else{
+				$this->set_page_error("Email not changed");
+			}
+		}
+		return $this->render_view("account/change_email.php");
+	}
+}
